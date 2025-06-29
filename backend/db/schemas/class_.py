12a157f029fd9f8731e07e8649fffea9ceb14627
zycon91 <fzikos@gmail.com>
2025-06-29 17:50:22 +0300

    fix: import Optional

--- backend/db/schemas/class_.py
+++ backend/db/schemas/class_.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import date, time
-from typing import List
+from typing import List, Optional
 
 
 class ClassSummary(BaseModel):
