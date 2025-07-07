4b4227a81997d7eadaa0004ada74966d2ef52e76
zycon91 <fzikos@gmail.com>
2025-07-07 17:27:22 +0300

    fix: cors origin for testing admin panel

--- backend/scripts/create_tables.py
+++ backend/scripts/create_tables.py
@@ -3,6 +3,7 @@ from db.models.admin import Admin
 from db.models.user import User
 from db.models.class_ import Class
 from db.models.booking import Booking
+from db.models.template_class import TemplateClass
 
 print("Creating tables:", Base.metadata.tables.keys())
 Base.metadata.create_all(bind=engine)
