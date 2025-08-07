6101fd7c3f423ecbccbeb11fa8e246218856181e
zycon91 <fzikos@gmail.com>
2025-08-07 13:15:48 +0300

    fix: cors and cookie options

--- backend/main.py
+++ backend/main.py
@@ -17,9 +17,6 @@ app.add_middleware(
         "https://app.breathepilatesefizikou.com",
         "https://breathe-pilates-admin-panel-dev.onrender.com",
         "https://breathe-pilates-booking-frontend.onrender.com",
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:6543"
     ],
     allow_credentials=True,
     allow_methods=["*"],
